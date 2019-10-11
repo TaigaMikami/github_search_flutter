f6a9e69f37c7eccf350f8e8ffffa4ff622e6a70d
TaigaMikami <mikamimikami60@gmail.com>
2019-10-12 00:53:58 +0900

    add search user text field

--- lib/pages/user_screen.dart
+++ lib/pages/user_screen.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
 
-class UserScreen extends StatelessWidget {
+class UserScreen extends StatefulWidget {
+  @override
+  _UserScreenState createState() => new _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: null,
+                decoration: InputDecoration.collapsed(hintText: "Enter Github Username"),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.search),
+                onPressed: null,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('User')
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: _buildTextComposer(),
+          ),
+          Divider(height: 2.0,),
+        ],
       ),
     );
   }
